6523adafcecf28c0b1b25083610166bb339c6c34
jpgianfaldoni <jpgianfaldoni@gmail.com>
2024-12-21 16:44:02 -0300

    create 2 models and improve model class

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,62 @@
 from flask import Flask, request, jsonify
+from model_handler import XGBoostModel
+from datetime import timedelta,datetime
 
 app = Flask(__name__)
 
 @app.route("/predict", methods=["POST"])
-def predict():
+def predict_next_4_weeks():
+    '''
+    Input example: input_obj = {
+            'store_data': [
+                {
+                    'Store': 1,
+                    'Dept': 1,
+                    'IsHoliday': True,
+                    'Type': 'A',
+                    'Size': 321312312312,
+                    'date': '26/02/2011'
+                },
+                        {
+                    'Store': 12,
+                    'Dept': 12,
+                    'IsHoliday': True,
+                    'Type': 'B',
+                    'Size': 1234,
+                    'date': '05/03/2011'
+                },
+                        {
+                    'Store': 1,
+                    'Dept': 1,
+                    'IsHoliday': True,
+                    'Type': 'A',
+                    'Size': 151315,
+                    'date': '12/03/2011'
+                },
+                        {
+                    'Store': 1,
+                    'Dept': 1,
+                    'IsHoliday': True,
+                    'Type': 'A',
+                    'Size': 151315,
+                    'date': '19/03/2011'
+                },
+            ]
+        }
+    '''
     try:
-       pass
+        data = request.get_json()
+        predictions = []
+        for store in data['store_data']:
+            predictions.append(model.predict(store))
+        return jsonify({"prediction": predictions}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
+    model_path = '../models/4_weeks_prediction_xgboost.pkl'
+    preprocessor_path = '../models/4_weeks_prediction_preprocessor.pkl'
+    model = XGBoostModel(model_path,preprocessor_path)
+    model.load_model()
+    model.load_preprocessor()
     app.run(debug=True)
--- src/app/model_handler.py
+++ src/app/model_handler.py
@@ -1,75 +1,94 @@
-import xgboost as xgb
 import pickle
-import numpy as np
-from datetime import datetime
+from datetime import timedelta,datetime
+import pandas as pd
+
 
 class XGBoostModel:
-    def __init__(self, model_path):
+    def __init__(self, model_path,preprocessor_path):
         self.model_path = model_path
+        self.preprocessor_path = preprocessor_path
         self.model = None
 
     def load_model(self):
-        """Load the model from the pickle file."""
         try:
             with open(self.model_path, "rb") as file:
                 self.model = pickle.load(file)
             print("Model loaded successfully.")
         except FileNotFoundError:
             raise Exception(f"Model file not found at {self.model_path}")
+        
+
+    def load_preprocessor(self):
+        try:
+            with open(self.preprocessor_path, "rb") as file:
+                self.preprocessor = pickle.load(file)
+            print("Preprocessor loaded successfully.")
+        except FileNotFoundError:
+            raise Exception(f"Preprocessor file not found at {self.preprocessor_path}")
 
     def predict(self, input_data):
-        """
-        Make predictions using the loaded model.
-        
-        Args:
-            input_data (list): List of lists (2D array) representing input features.
-        Returns:
-            list: Predictions for the input data.
-        """
-        if self.model is None:
-            raise Exception("Model is not loaded. Call load_model() first.")
-        dmatrix = xgb.DMatrix(np.array(input_data))
-        predictions = self.model.predict(dmatrix)
+        # '''{
+        # 'Store': number,
+        # 'Dept': string,
+        # 'IsHoliday': boolean,
+        # 'Type': string,
+        # 'Size':number,
+        # 'date':timestamp
+        # }'''
+        processed_data = self.process_input(input_data)
+        input_data_transformed = self.preprocessor.transform(processed_data)
+        predictions = self.model.predict(input_data_transformed)
         return predictions.tolist()
     
-    def process_input(self, data):
-        '''{
-        'Store': number,
-        'Dept': string,
-        'IsHoliday': boolean,
-        'Type': string,
-        'Size':number,
-        'date':timestamp,
-        'MarkDown2': float,
-        'MarkDown3': float,
-        'MarkDown4': float,
-        'MarkDown5': float,
-        'CPI': float,
-        'Unemployment': float
-        }'''
+
+    def create_lagged_features(self,df, store, dept, date):
+        df['Date'] = pd.to_datetime(df['Date'])
+        date = pd.to_datetime(date)
+        reference_date = date - timedelta(weeks=4)
+        
+        df_filtered = df[(df['Store'] == store) & (df['Dept'] == dept)]
+        if df_filtered.empty:
+            raise ValueError("No data found for the specified store and department.")
+        
+        shiftable_columns = ['Weekly_Sales', 'Temperature', 'MarkDown1', 'Fuel_Price',
+                            'MarkDown2', 'MarkDown3', 'MarkDown4', 
+                            'MarkDown5', 'CPI', 'Unemployment']
+        
+        lags = [4, 8, 16, 32]
+        lagged_features = pd.DataFrame(index=[0])
+        
+        for lag in lags:
+            for col in shiftable_columns:
+                lagged_col_name = f'lag_{lag}_{col}'
+                lagged_value = df_filtered[col].shift(lag).iloc[-1]
+                lagged_features[lagged_col_name] = [lagged_value]
+        
+        week_number = reference_date.isocalendar()[1]  
+        for col in shiftable_columns:
+            avg_col_name = f'{col}_historical_week_avg'
+            historical_avg = df_filtered[df_filtered['Date'].dt.isocalendar().week == week_number][col].mean()
+            lagged_features[avg_col_name] = [historical_avg]
+        return lagged_features
+    
+    def process_input(self, data):        
         parsed_date = datetime.strptime(data['date'], "%d/%m/%Y")
 
-        data['week'] = parsed_date.dt.isocalendar().week  
-        data['month'] = parsed_date.month          
-        data['day'] = parsed_date.day               
-
-        timestamp = parsed_date.timestamp()  
-        data_list = [
-            data['Store'],
-            data['Dept'], 
-            bool(data['IsHoliday']),  
-            data['Type'],  
-            data['Size'],
-            timestamp,  
-            data['week'],
-            data['month'],
-            data['day'],
-            data['MarkDown2'],
-            data['MarkDown3'],
-            data['MarkDown4'],
-            data['MarkDown5'],
-            data['CPI'],
-            data['Unemployment']
-        ]
-        return
-        
+        data['week'] = parsed_date.isocalendar().week
+        data['month'] = parsed_date.month
+        data['day'] = parsed_date.day  
+        original_data = {
+            'Date': parsed_date,
+            'Store': data['Store'],
+            'Dept': data['Dept'], 
+            'IsHoliday': bool(data['IsHoliday']),  
+            'Type': data['Type'],  
+            'Size': data['Size'],
+            'week': data['week'],
+            'month': data['month'],
+            'day': data['day']
+        }
+        df = pd.read_csv('../../data/combined_data.csv')
+        df = self.create_lagged_features(df, original_data['Store'],original_data['Dept'],original_data['Date'])
+        for key, value in original_data.items():
+            df[key] = value
+        return df
